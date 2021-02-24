8d7fb86a90788c54cc7571de8d63078952817a14
adimitriev <a.dimitriev.86@yandex.ru>
2021-02-24 18:47:30 +0300

    Функция на эквивалентность двух массивов

--- src/algoritms/equals-array.ts
+++
@@ -1,4 +0,0 @@
-export function equalsArray<T = any>(arr1: T[], arr2: T[], equalFunc: (a: T, b: T) => boolean = (a, b) => a === b) {
-    return arr1.length === arr2.length &&
-        arr1.every((item, i) => equalFunc(item, arr2[i]));
-}
